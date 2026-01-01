26e4fdee06760e8fd7f2cf72944d1091434fd946
Moi10legend <moisesflopesf@gmail.com>
2026-01-01 17:52:32 -0300

    adicionando o frontend como origin válida no middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -35,6 +35,7 @@ origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://vertex-bank-api.onrender.com",
+    "https://vertex-bank-blue.vercel.app"
 ]
 
 app.add_middleware(
